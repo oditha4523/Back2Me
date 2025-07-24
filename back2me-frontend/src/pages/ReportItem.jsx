006241cfd98a800f83a8dc9ea7b2a1139d892d9f
oditha4523 <odithachamod@gmail.com>
2025-07-24 20:11:18 +0530

    created frontend file structure

--- back2me-frontend/src/pages/ReportItem.jsx
+++ back2me-frontend/src/pages/ReportItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const ReportItem = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default ReportItem
